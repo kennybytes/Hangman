753de19457d1eef5877d9f5374adb6546ea01923
kennyluong92@gmail.com <kenny@Silvia-SX260.(none)>
2012-02-24 23:19:45 -1000

    commit two

---
+++ hangman.java
@@ -0,0 +1,19 @@
+// 
+//
+//	Hangman in Java
+//	Kenny Luong
+//	2.24.12
+//
+//	Main hangman driver
+//
+//
+
+
+class HangMan{
+
+public static void main(String[] args){ 
+	System.out.println("We're playing hangman now!");
+
+}
+
+}
