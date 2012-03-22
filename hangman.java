fb104c2f4a8bd6a85957171103065ab9400b2f85
kennyluong92@gmail.com <kenny@Silvia-SX260.(none)>
2012-03-21 14:37:13 -1000

    made the code work again

--- hangman.java
+++ hangman.java
@@ -13,8 +13,6 @@ class HangMan{
 
 public static void main(String[] args){ 
 	System.out.println("We're playing hangman now!");
-	Robot r = new Robot();	
-	r.mouseMove(40,40);
 	
 //test comments
 }
